d6eec119d7d10220460c5593743ebc3c21752207
YDH <prettyhoon@gmail.com>
2018-10-18 11:09:14 +0900

    the first version

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace HelloWorld //이것은 네임스페이스이고 이름은 HelloWorld입니다.
+{
+    class Program
+    {
+        //프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("HelloWorld  이름"); 
+                System.Console.ReadKey(); // 콘솔에서 키를 받기위해 대기
+                return ;
+              
+            }
+            System.Console.WriteLine("Hello {0}", args[0]);// args[0] 0번째를 호출
+            System.Console.ReadKey();
+            
+        }
+      
+    }
+}
